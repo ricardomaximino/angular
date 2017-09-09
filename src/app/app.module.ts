7153a6b66a2d194a7bbd43a16f5f0ff2cfe587d5
ricardomaximino2 <ricardomaximino@gmail.com>
2017-09-09 03:11:43 +0200

    Ajusting atom with git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
